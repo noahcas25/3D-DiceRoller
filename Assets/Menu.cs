6e3a69d4dbbf7eff9d15bb24ab0615ca86fa0cce
Noah <Noah_castro25@yahoo.com>
2021-04-20 05:11:23 -0700

    Set up Utilities App with 3D Dice Roll
    Installed Probuilder, texturized table and dice.
    Menu script, button functionality

---
+++ Assets/Menu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    public GameObject dice;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int rand1 = Random.Range(30, 180);
+        int rand2 = Random.Range(30, 180);
+        int rand3 = Random.Range(30, 180);
+
+        dice.transform.Rotate(new Vector3(rand1, rand2, rand3)); 
+    }
+
+    public void Roll() {
+        
+        SceneManager.LoadScene("3D Dice");
+    }
+
+    public void Back() {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
